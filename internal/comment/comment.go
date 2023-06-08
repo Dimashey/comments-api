a39931444d0579db147781840f0bf86008f0a495
Dimashey <sheykodmitriyjob@gmail.com>
2023-06-08 21:02:21 +0300

    add abstract methods to comment service

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -2,7 +2,13 @@ package comment
 
 import (
 	"context"
+	"errors"
 	"fmt"
+)
+
+var (
+	ErrFetchingComment = errors.New("failed to fetch comment by id")
+	ErrNotImplemented  = errors.New("not implmented")
 )
 
 // Comment - a representation of the comment
@@ -37,8 +43,20 @@ func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
 	if err != nil {
 		fmt.Println(err)
 
-		return Comment{}, err
+		return Comment{}, ErrFetchingComment
 	}
 
 	return cmt, nil
+}
+
+func (s *Service) UpdateComment(ctx context.Context, cmt Comment) error {
+	return ErrFetchingComment
+}
+
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) CreateComment(ctx context.Context, cmt Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
 }
